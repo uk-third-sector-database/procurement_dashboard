80a625b8ca66c196cd1f7de16a5c1cda35324959
Mihaela Duta <mihaela.duta@dtc.ox.ac.uk>
2025-09-18 13:02:19 +0100

    Add project information section.

---
+++ assets/privacy.md
@@ -0,0 +1,3 @@
+# Privacy policy
+
+Text here.
--- src/gui/sidebar.py
+++ src/gui/sidebar.py
@@ -7,6 +7,9 @@ import streamlit as st
 
 ASSETS_DIR = Path(__file__).parent.parent.parent / "assets"
 
+ABOUT_FILE = ASSETS_DIR / "about.md"
+PRIVACY_FILE = ASSETS_DIR / "privacy.md"
+
 LOGOS = [
     # ASSETS_DIR / "oxford_logo.png",
     [ASSETS_DIR / "tsrc_square.jpg", "https://www.birmingham.ac.uk/research/tsrc"],
@@ -33,7 +36,7 @@ def image_with_link(image_path: Path, link_url: str, width: int = 100) -> None:
 def display() -> None:
     """Build the sidebar for the Streamlit app."""
 
-    st.sidebar.title("Procurement Dashboard")
+    st.sidebar.header("UK Third Sector Database")
 
     logo_columns = st.sidebar.columns(3)
     with logo_columns[0]:
@@ -48,5 +51,16 @@ def display() -> None:
         image_with_link(LOGOS[3][0], LOGOS[3][1])
     with logo_columns[1]:
         image_with_link(LOGOS[4][0], LOGOS[4][1])
+
+    with st.sidebar.expander("Project information", expanded=False):
+        with st.popover("About the project"):
+            st.markdown(ABOUT_FILE.read_text(encoding="utf-8"))
+        with st.popover("Privacy policy"):
+            st.markdown(PRIVACY_FILE.read_text(encoding="utf-8"))
+
+        with st.popover("Resources"):
+            st.link_button("Project GitHub page", "https://uk-third-sector-database.github.io/")
+            st.link_button("Raw spine files", "https://uk-third-sector-database.github.io/data/")
+            st.link_button("How to use the dataset", "https://uk-third-sector-database.github.io/")
 
     st.sidebar.divider()
